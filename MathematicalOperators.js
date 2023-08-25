0d537d66a8be01de2fe4dc1db5a11cfb1d8fae6e
Hasan Patel <Hasan8459@users.noreply.github.com>
2023-08-25 14:41:48 +0530

    Mathematical operator problem solving in ob

---
+++ MathematicalOperators.js
@@ -0,0 +1,6 @@
+console.log(number*50);
+console.log(X*3+10); console.log(one*two*three*four*five*six);
+console.log(one+two+three+four+five);
+console.log(N**3);
+console.log(one*one+two*two+three*three);
+console.log(X*3+10); 
